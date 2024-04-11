cece786f5ef4a10417d1072cadc8cc903ec1abc3
Arthur ALO <alopezocampo@hotmail.com>
2024-04-11 16:25:52 -0600

    Primer version
    Primer version a produccion

---
+++ sewing/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Sewing',
+    'version': '0.1.42',
+    'summary': 'Módulo de control de requerimientos de costureria',
+    'author': 'Quality Systems',
+    'website': 'https://www.qualitysystems.com',
+    'category':'Services',
+    'depends': [
+        'hr',
+        'mail',
+        'crm',
+        'sale',
+    ],
+    'data': [
+        'reports/ir_actions_report.xml',
+        'reports/ticket_invoice_report.xml',
+        'views/views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
